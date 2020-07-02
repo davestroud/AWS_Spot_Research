cbd8b0e0afe09065c9b9cb157ff603105358e45a
davestroud <david@davidstroud.me>
2020-07-02 14:40:38 -0500

    json file reading in properly

--- LSTM.py
+++ LSTM.py
@@ -1,5 +1,8 @@
 # Playground file
 import json
 
-with open("results.json", "r") as read_file:
-    data = json.load(read_file)
+with open('spot.json', 'r', encoding="utf-16") as f:
+    json_text = f.read()
+json.loads(json_text)
+
+print(json_text)
