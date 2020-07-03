e073127af9689efe4f7c0e863ea753ab938a3231
davestroud <david@davidstroud.me>
2020-07-02 19:39:52 -0500

    remove old sql code

--- sql_wb_test.py
+++ sql_wb_test.py
@@ -1,28 +1,3 @@
-# import mysql.connector
-# from mysql.connector import Error
-
-
-# def connect():
-#     """ Connect to MySQL database """
-#     conn = None
-#     try:
-#         conn = mysql.connector.connect(host='AWSRDS',
-#                                        database='aws-spot',
-#                                        user='root',
-#                                        password='Barron12#')
-#         if conn.is_connected():
-#             print('Connected to MySQL database')
-
-#     except Error as e:
-#         print(e)
-
-#     finally:
-#         if conn is not None and conn.is_connected():
-#             conn.close()
-
-
-# if __name__ == '__main__':
-#     connect()
 
 import mysql.connector
 
