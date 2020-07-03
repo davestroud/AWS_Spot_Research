5d7e366983460f47072287c994ccde6620dbbc03
davestroud <david@davidstroud.me>
2020-07-02 19:37:12 -0500

    sql_wb file working

---
+++ sql_wb_test.py
@@ -0,0 +1,39 @@
+# import mysql.connector
+# from mysql.connector import Error
+
+
+# def connect():
+#     """ Connect to MySQL database """
+#     conn = None
+#     try:
+#         conn = mysql.connector.connect(host='AWSRDS',
+#                                        database='aws-spot',
+#                                        user='root',
+#                                        password='Barron12#')
+#         if conn.is_connected():
+#             print('Connected to MySQL database')
+
+#     except Error as e:
+#         print(e)
+
+#     finally:
+#         if conn is not None and conn.is_connected():
+#             conn.close()
+
+
+# if __name__ == '__main__':
+#     connect()
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "aws-spot.cbxr4ccqwh4f.us-east-1.rds.amazonaws.com", 
+    user = "admin",
+    password = "Barron12#"
+)
+
+print(mydb)
+
+
+
+
