91e8f8847d012eb198fca80b2719069824c169de
davestroud <david@davidstroud.me>
2020-07-03 10:04:00 -0500

    sql commands

--- sql_wb_test.py
+++ sql_wb_test.py
@@ -2,9 +2,9 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host = "aws-spot.cbxr4ccqwh4f.us-east-1.rds.amazonaws.com", 
-    user = "admin",
-    password = "Barron12#"
+    host = "#############", 
+    user = "#########",
+    password = "################"
 )
 
 print(mydb)
