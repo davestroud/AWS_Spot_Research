07fd481f52b6baf812788b04f578221d6dbb7d68
davestroud <david@davidstroud.me>
2020-07-23 19:14:09 -0500

    set up lstm_utils file

---
+++ LSTM/lstm_utils.py
@@ -0,0 +1,12 @@
+## for pytorch
+import torch
+import torch.nn as nn
+
+# for graphics
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# for data
+import numpy as np
+import pandas as pd
