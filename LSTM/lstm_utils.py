c570cf7e8cb86e303564cb2112c447132d421bc9
davestroud <david@davidstroud.me>
2020-07-24 10:01:55 -0500

    wrote code to normalize passenger category

--- LSTM/lstm_utils.py
+++ LSTM/lstm_utils.py
@@ -5,8 +5,10 @@ import torch.nn as nn
 # for graphics
 import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
 
 # for data
 import numpy as np
 import pandas as pd
+
+# for data normalization
+from sklearn.preprocessing import MinMaxScaler
