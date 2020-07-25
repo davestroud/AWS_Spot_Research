5024a5917f5e0bbd7ddb7193297003916f57b770
davestroud <david@davidstroud.me>
2020-07-25 13:21:13 -0500

    add create_inout_sequences

--- LSTM/.ipynb_checkpoints/lstm_utils-checkpoint.py
+++ LSTM/.ipynb_checkpoints/lstm_utils-checkpoint.py
@@ -12,3 +12,22 @@ import pandas as pd
 
 # for data normalization
 from sklearn.preprocessing import MinMaxScaler
+
+"""
+    The create_inout_sequences function will accept
+    raw input data and return a list of tuples.
+    In each tuple, the first element will contain
+    a list of 12 items corresponding to the number 
+    of passengers traveling in 12 months. The second
+    tuple element will contain one item; ie, 
+    the number of passengers inthe 12+1st month.
+
+"""
+def create_inout_sequences(input_data, tw):
+    inout_seq = []
+    for i in range(L-tw):
+        train_seq = input_data[i:i+tw]
+        train_label = input_data[i+tw:i+tw+1]
+        input_seq.append((train_seq, train_label))
+    return inout_seq
+    
