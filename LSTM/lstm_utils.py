47e48a8c5426c3a404d69c88247be0a35506a20d
dstroud <david@davidstroud.me>
2020-07-28 12:31:09 -0500

    add remainder of lstm file

--- LSTM/lstm_utils.py
+++ LSTM/lstm_utils.py
@@ -20,7 +20,7 @@ from sklearn.preprocessing import MinMaxScaler
     a list of 12 items corresponding to the number 
     of passengers traveling in 12 months. The second
     tuple element will contain one item; ie, 
-    the number of passengers inthe 12+1st month.
+    the number of passengers in the 12+1st month.
 
 """
 def create_inout_sequences(input_data, tw):
@@ -31,3 +31,25 @@ def create_inout_sequences(input_data, tw):
         train_label = input_data[i+tw:i+tw+1]
         inout_seq.append((train_seq ,train_label))
     return inout_seq
+
+"""
+    Create LSTM Model
+
+"""
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                            torch.zeros(1,1,self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
+
